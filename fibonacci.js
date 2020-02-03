77d052235a10a33056858b992b2e50ba99449664
faurewang <wangli0504@outlook.com>
2020-02-03 11:22:52 +0100

    displayed fibonacci in the displayer

--- fibonacci.js
+++ fibonacci.js
@@ -3,13 +3,20 @@ function displayFibonacci() {
     let n1 = 1;
     let ntemp = 0;
 
-    console.log(n0);
-    console.log(n1);
+    // document.write(n0);
+    // document.write('<br>');
+    // document.write(n1);
+    // document.write('<br>');
+    document.getElementById("fibonacci").innerHTML = document.getElementById("fibonacci").innerHTML + "<br>" + n0;
+    document.getElementById("fibonacci").innerHTML = document.getElementById("fibonacci").innerHTML + "<br>" + n1;
     for (let i = 0; i < 98; i++) {
         ntemp = n0 + n1;
         n0 = n1;
         n1 = ntemp;
-        console.log(n1);
+        // document.write(n1);
+        document.getElementById("fibonacci").innerHTML = document.getElementById("fibonacci").innerHTML + "<br>" + n1;
+        // document.write('<br>');
+        // console.log(n1);
     }
 }
 
