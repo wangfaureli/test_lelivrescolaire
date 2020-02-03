cb0f8a8d4b07facd03cd93024e1f77b062423d63
faurewang <wangli0504@outlook.com>
2020-02-03 15:22:45 +0100

    finish fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,21 +1,38 @@
+let n0 = 0;
+let n1 = 1;
+
 function displayFibonacci() {
-
-    let n0 = 0;
-    let n1 = 1;
     let ntemp = 0;
-    let u = 0;
+    let rgbHue = 255 / 99;
+    let fibonacciDiv = document.querySelector(".fibonacci");
 
-    for (let i = 0; i < 98; i++) {
-        if (u == 0) {
-            document.getElementById("fibonacci").innerHTML = "<div style='color: rgb("+u+","+u+","+u+")'>" + document.getElementById("fibonacci").innerHTML + "<br>" + n0 + "</div>";
-            document.getElementById("fibonacci").innerHTML = "<div style='color: rgb("+u+","+u+","+u+")'>" + document.getElementById("fibonacci").innerHTML + "<br>" + n1 + "</div>";
+    fibonacciDiv.innerHTML = "";
+    for (let i = 0; i < 100; i++) {
+        let div = document.createElement("div");
+
+        if (n0 == 0) {
+            i = 2;
+            let firstDiv = document.createElement("div");
+            let secondDiv = document.createElement("div");
+            firstDiv.innerHTML = n0;
+            secondDiv.innerHTML = n1;
+            secondDiv.style.color = `rgb(${rgbHue}, ${rgbHue}, ${rgbHue})`;
+            fibonacciDiv.appendChild(firstDiv);
+            fibonacciDiv.appendChild(secondDiv);
+            rgbHue += 255 / 99;
         }
         ntemp = n0 + n1;
         n0 = n1;
         n1 = ntemp;
-        document.getElementById("fibonacci").innerHTML = "<div style='color: rgb("+u+","+u+","+u+")'>" + document.getElementById("fibonacci").innerHTML + "<br>" + n1 + "</div>";
-        u = u + 3;
+        div.innerHTML = n1;
+        div.style.color = `rgb(${rgbHue}, ${rgbHue}, ${rgbHue})`;
+        fibonacciDiv.appendChild(div);
+        rgbHue += 255 / 99;
     }
 }
 
-displayFibonacci();
+function main() {
+    displayFibonacci();
+}
+
+main();
